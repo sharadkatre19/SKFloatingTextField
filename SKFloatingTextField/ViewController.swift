b012bffb719cad491d0f41be89a8c1c280e56b96
Sharad Katre <sharadkatre@mipl.local>
2019-01-22 15:55:01 +0530

    Initial Commit

---
+++ SKFloatingTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SKFloatingTextField
+//
+//  Created by Sharad Katre on 22/01/19.
+//  Copyright © 2019 Sharad Katre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
